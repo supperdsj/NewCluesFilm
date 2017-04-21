07e57877bef878904d9674c966ff4ffb5db48566
dongsj <232171773@qq.com>
2017-04-21 19:19:08 +0800

    p4 finish

--- src/app/p4/p4.component.ts
+++ src/app/p4/p4.component.ts
@@ -12,6 +12,20 @@ export class P4Component implements OnInit {
     }
 
     ngOnInit() {
+        setTimeout(() => {
+            $($('.subBox')[0]).attr('class', 'subBox l2 a1');
+            $($('.subBox')[1]).attr('class', 'subBox l2 a2');
+            $($('.subBox')[2]).attr('class', 'subBox l1 a3');
+            setTimeout(() => {
+                $('.bline').attr('class', 'bline blinea');
+            }, 200);
+            setTimeout(() => {
+                $('.cat').css({
+                    'bottom': '0',
+                    'left': '-0.64rem'
+                });
+            }, 500);
+        }, 1300);
     }
 
     bindViewTap() {
